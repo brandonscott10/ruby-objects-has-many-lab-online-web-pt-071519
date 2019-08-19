831cc5819abc90e7fc486cc1ecfd61ae29c7981f
Brandon Parris <brandonparrisguy@gmail.com>
2019-08-19 02:23:39 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,10 +1,11 @@
 class Artist 
   attr_accessor :name
   attr_reader :songs
-  @@songs = []
+  @@all = []
   
   def initialize(name)
     @name = name 
+    @songs = songs 
   end
   
   def songs
